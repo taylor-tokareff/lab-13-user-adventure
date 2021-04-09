59a67e0650e31c017f78638aa7439fb7aa0b58d5
taylor-tokareff <taylortokareff@gmail.com>
2021-04-09 14:48:12 -0700

    quest data 1/3 complete

--- home/make-user.js
+++ home/make-user.js
@@ -3,7 +3,7 @@ function makeUser(formData) {
         name: formData.get('name'),
         userClass: formData.get('class'),
         hp: 100,
-        gold: 0,
+        gold: 5,
         completed: {}
     };
 
---
+++ map/map.js
@@ -0,0 +1,1 @@
+import { getUser } from '../data/stringy.js';
