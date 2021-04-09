a2ad74c3cf0535d82062db20dc35179e4210e830
taylor-tokareff <taylortokareff@gmail.com>
2021-04-09 16:33:11 -0700

    finished load-profile function

---
+++ data/load-profile.js
@@ -0,0 +1,27 @@
+import { getUser } from './stringy.js';
+import isDead from './is-dead.js';
+
+function loadProfile() {
+    const name = document.getElementById('name');
+    const meat = document.getElementById('meat');
+    const hp = document.getElementById('hp');
+    const gold = document.getElementById('gold');
+
+    const user = getUser();
+
+    name.textContent = user.name;
+    meat.src = '../img/' + user.userClass + '.png';
+    gold.textContent = user.gold;
+
+    if (isDead(user)) {
+        hp.textContent = 'You succumb to your wounds. There is no glory in death, only an eternity of carnage and slaughter, and the laughter of thirsting gods...';
+    }
+
+    else {
+        // otherwise show the user hp
+        hp.textContent = user.hp;
+    }
+
+}
+
+export default loadProfile;
--- map/map.js
+++ map/map.js
@@ -1,1 +1,2 @@
 import { getUser } from '../data/stringy.js';
+import quests from '../data/quest-data.js';
