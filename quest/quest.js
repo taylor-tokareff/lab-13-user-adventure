d891da5ba2cf52f7df13c0ad5f289c50657772ab
taylor-tokareff <taylortokareff@gmail.com>
2021-04-12 16:06:17 -0700

    fixed multiple choice issue

--- quest/quest.js
+++ quest/quest.js
@@ -2,6 +2,7 @@ import loadProfile from '../data/load-profile.js';
 import { getUser, saveUser } from '../data/stringy.js';
 import quests from '../data/quest-data.js';
 import { findById, statUpdater, choiceCreator } from '../data/quest-functions.js';
+import { questCompleter } from '../quest-status/quest-status.js';
 
 loadProfile();
 
@@ -23,6 +24,7 @@ const choices = document.getElementById('choices');
 const result = document.getElementById('result');
 const resultDescription = document.getElementById('result-description');
 
+
 title.textContent = quest.title;
 image.src = quest.image;
 description.textContent = quest.description;
@@ -33,8 +35,10 @@ for (let index = 0; index < quest.choices.length; index++) {
     choices.appendChild(choiceDOM);
 }
 
-choiceForm.addEventListener('submit', function (event) {
+choiceForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    choiceForm.classList.add('hidden');
+    result.classList.remove('hidden');
 
     const formData = new FormData(choiceForm);
 
@@ -42,15 +46,18 @@ choiceForm.addEventListener('submit', function (event) {
 
     const choice = findById(quest.choices, choiceId);
 
+    resultDescription.textContent = choice.result;
+
     const user = getUser();
 
     statUpdater(choice, quest.id, user);
 
     saveUser(user);
 
-    choiceForm.classList.add('hidden');
-    result.classList.remove('hidden');
-    resultDescription.textContent = choice.result;
+
+
 
     loadProfile();
+
+
 });
